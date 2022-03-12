0ba571cc282da6d1a8dcb66fc2e1d3b9df2a88f4
willianchu <willianchu@hotmail.com>
2022-03-12 14:46:29 -0800

    create user view and empty user controller

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,11 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
+  # login and new user
+  get '/signup', to: 'users#new'
+  post '/users', to: 'users#create'	
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
