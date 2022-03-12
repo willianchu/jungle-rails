88ffad282c6adbfe6bd10a0a3428cd54d682c5c8
willianchu <willianchu@hotmail.com>
2022-03-12 14:27:31 -0800

    do the model and migration of users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625062916) do
+ActiveRecord::Schema.define(version: 20220312222344) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -52,5 +52,13 @@ ActiveRecord::Schema.define(version: 20160625062916) do
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
 end
