3a2f61e660be246b19869825551addec43e4d74d
willianchu <willianchu@hotmail.com>
2022-03-12 20:39:16 -0800

    add list new and create admin/categories

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
     resources :sales, only: [:index, :new]
   end
 
